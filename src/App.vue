4f9889569dd652b300a08acb11d0026164fdd340
VirtusXVI <manuel.aveta2001@gmail.com>
2022-07-05 17:19:34 +0200

    repsitory creation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainHeader
   }
 }
 </script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="MainHeader">
+    <h1>Welcome to Your Vue.js App</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MainHeader',
+  props: {
+    variable1: String
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
